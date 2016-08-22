1716d5bd2ac27ade5eae5c2735da8f24144ce0ad
John Gray <johnny@johnnygray.co.uk>
2016-08-22 14:03:14 +0100

    - Mapping can now find edge and corners of square rooms. This probably needs expanded to more complex shapes than 4 cornered polygons.

--- Assets/Scripts/Gameplay/Rooms/RoomInfo.cs
+++ Assets/Scripts/Gameplay/Rooms/RoomInfo.cs
@@ -2,9 +2,16 @@
 
 public class RoomInfo
 {
-	//GridInfo[] m_acRoomGrid;
-
 	List<GridInfo> m_lstRoomGrid = new List<GridInfo>();
+	GridCornerCoordinates m_cGridCornerCoordinates = new GridCornerCoordinates();
+
+	class GridCornerCoordinates
+	{
+		public int m_nXMin = int.MaxValue;
+		public int m_nXMax = int.MinValue;
+		public int m_nYMin = int.MaxValue;
+		public int m_nYMax = int.MinValue;
+	}
 
 	public void RegisterRoomGrid(GridInfo[] acRoomGrid)
 	{
@@ -14,6 +21,8 @@ public class RoomInfo
 		{
 			m_lstRoomGrid[nGridInfo].SetInRoom();
 		}
+
+		FindGridCornerCoordinates();
 	}
 
 	public void DeregisterRoomGrid(GridInfo[] acRoomGrid)
@@ -28,4 +37,72 @@ public class RoomInfo
 			}
 		}
     }
+
+	void FindGridCornerCoordinates()
+	{
+		for (int nGridInfo = 0; nGridInfo < m_lstRoomGrid.Count; nGridInfo++)
+		{
+			GridInfo cGridInfo = m_lstRoomGrid[nGridInfo];
+
+			if (cGridInfo.GridX < m_cGridCornerCoordinates.m_nXMin)
+			{
+				m_cGridCornerCoordinates.m_nXMin = cGridInfo.GridX;
+			}
+
+			if (cGridInfo.GridX > m_cGridCornerCoordinates.m_nXMax)
+			{
+				m_cGridCornerCoordinates.m_nXMax = cGridInfo.GridX;
+			}
+
+			if (cGridInfo.GridY < m_cGridCornerCoordinates.m_nYMin)
+			{
+				m_cGridCornerCoordinates.m_nYMin = cGridInfo.GridY;
+			}
+
+			if (cGridInfo.GridY > m_cGridCornerCoordinates.m_nYMax)
+			{
+				m_cGridCornerCoordinates.m_nYMax = cGridInfo.GridY;
+			}
+		}
+	}
+
+	GridInfo[] FindGridCorners()
+	{
+		GridInfo[] acGridCorners = new GridInfo[4];
+
+		int nCount = 0;
+
+		for (int nGridInfo = 0; nGridInfo < m_lstRoomGrid.Count; nGridInfo++)
+		{
+			GridInfo cGridInfo = m_lstRoomGrid[nGridInfo];
+
+			if ((cGridInfo.GridX == m_cGridCornerCoordinates.m_nXMin || cGridInfo.GridX == m_cGridCornerCoordinates.m_nXMax) && (cGridInfo.GridY == m_cGridCornerCoordinates.m_nYMin || cGridInfo.GridY == m_cGridCornerCoordinates.m_nYMax))
+			{
+				acGridCorners[nCount] = cGridInfo;
+
+				nCount++;
+			}
+		}
+
+		return acGridCorners;
+	}
+
+	GridInfo[] FindGridEdge()
+	{
+		List<GridInfo> lstGridEdge = new List<GridInfo>();
+
+		for (int nGridInfo = 0; nGridInfo < m_lstRoomGrid.Count; nGridInfo++)
+		{
+			GridInfo cGridInfo = m_lstRoomGrid[nGridInfo];
+
+			if (cGridInfo.GridX == m_cGridCornerCoordinates.m_nXMin || cGridInfo.GridX == m_cGridCornerCoordinates.m_nXMax || cGridInfo.GridY == m_cGridCornerCoordinates.m_nYMin || cGridInfo.GridY == m_cGridCornerCoordinates.m_nYMax)
+			{
+				cGridInfo.UnmappingHighlight();
+
+				lstGridEdge.Add(cGridInfo);
+			}
+		}
+
+		return lstGridEdge.ToArray();
+	}
 }
