7b086c03a8fad5c64e62b7a69529b0dbb9d6ae07
John Gray <johnny@johnnygray.co.uk>
2016-09-04 11:42:57 +0100

    - Work to fade the intensity of the sunlight based on a graph 0 -> 1 (0 is midday, 1 is midnight).

--- Assets/DayNightCycle/DayNightCycleSun.cs
+++ Assets/DayNightCycle/DayNightCycleSun.cs
@@ -11,11 +11,16 @@ public class DayNightCycleSun : MonoBehaviour
 	[Tooltip("Duration of a full revolution around the FocusPoint in seconds.")]
 	float RevolutionDuration = 10.0f;
 
+	[SerializeField]
+	AnimationCurve SunLightIntensity;
+
 	float m_fRevolutionEndTime = 0.0f;
 
-	float m_fRevolutionAngle = 360.0f;
+	const float m_fRevolutionAngle = 360.0f;
 
-	float m_fStageSegmentAdjustment = 0.0f;
+	float m_fPercentageSegmentAdjustment = 0.0f;
+
+	Light m_cSunLight;
 
 	enum Stage
 	{
@@ -27,25 +32,20 @@ public class DayNightCycleSun : MonoBehaviour
 
 	void Awake()
 	{
-		m_fStageSegmentAdjustment = (m_fRevolutionAngle / 8.0f) / m_fRevolutionAngle;
+		m_cSunLight = GetComponent<Light>();
+
+		m_fPercentageSegmentAdjustment = (m_fRevolutionAngle / 8.0f) / m_fRevolutionAngle;
 
 		// Get the start angle. We assume that vector3.up is "Midday".
-		float fAngle = Angle(Vector3.up, (transform.position - FocusPoint.transform.position).normalized, Vector3.right);
+		float fAngle = GetRevolutionAngle();
 
 		// Sort the time as if it had started and run on to that position.
 		m_fRevolutionEndTime = Time.time + (RevolutionDuration - (fAngle / m_fRevolutionAngle) * RevolutionDuration);
 	}
 
-	public static float Angle(Vector3 v1, Vector3 v2, Vector3 n)
+	float GetRevolutionAngle()
 	{
-		float fAngle = Mathf.Atan2(Vector3.Dot(n, Vector3.Cross(v1, v2)), Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
-
-		if (fAngle < 0.0f)
-		{
-			fAngle = 360.0f + fAngle;
-		}
-
-		return fAngle;
+		return MathUtils.Angle(Vector3.up, (transform.position - FocusPoint.transform.position).normalized, Vector3.right);
 	}
 
 	void Update()
@@ -56,6 +56,8 @@ public class DayNightCycleSun : MonoBehaviour
 		}
 
 		Debug.Log(GetStage().ToString());
+
+		m_cSunLight.intensity = SunLightIntensity.Evaluate(GetRevolutionPrecentageComplete());
 
 		transform.RotateAround(FocusPoint.transform.position, Vector3.right, (Time.deltaTime / RevolutionDuration) * m_fRevolutionAngle);
 		transform.LookAt(FocusPoint.transform, Vector3.up);
@@ -68,21 +70,21 @@ public class DayNightCycleSun : MonoBehaviour
 
 	Stage GetStage()
 	{
-		float fPercentage = (RevolutionDuration - (m_fRevolutionEndTime - Time.time)) / RevolutionDuration;
+		float fPercentage = GetRevolutionPrecentageComplete();
 
-		if (fPercentage < 0.25f - m_fStageSegmentAdjustment)
+		if (fPercentage < 0.25f - m_fPercentageSegmentAdjustment)
 		{
 			return Stage.Midday;
 		}
-		else if (fPercentage < 0.5f - m_fStageSegmentAdjustment)
+		else if (fPercentage < 0.5f - m_fPercentageSegmentAdjustment)
 		{
 			return Stage.Dusk;
 		}
-		else if (fPercentage < 0.75f - m_fStageSegmentAdjustment)
+		else if (fPercentage < 0.75f - m_fPercentageSegmentAdjustment)
 		{
 			return Stage.Midnight;
 		}
-		else if (fPercentage < 1.0f - m_fStageSegmentAdjustment)
+		else if (fPercentage < 1.0f - m_fPercentageSegmentAdjustment)
 		{
 			return Stage.Dawn;
 		}
@@ -90,5 +92,13 @@ public class DayNightCycleSun : MonoBehaviour
 		{
 			return Stage.Midday;
 		}
+	}
+
+	float GetRevolutionPrecentageComplete()
+	{
+		float fPercentage = (RevolutionDuration - (m_fRevolutionEndTime - Time.time)) / RevolutionDuration;
+
+		// Subtract the segement adjustment to shift the quarters on the clock face.
+		return fPercentage;
 	}
 }
---
+++ Assets/Scripts/Utilities/MathUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathUtils : MonoBehaviour
+{
+	public static float Angle(Vector3 v1, Vector3 v2, Vector3 n)
+	{
+		float fAngle = Mathf.Atan2(Vector3.Dot(n, Vector3.Cross(v1, v2)), Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
+
+		if (fAngle < 0.0f)
+		{
+			fAngle = 360.0f + fAngle;
+		}
+
+		return fAngle;
+	}
+}
